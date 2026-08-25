dd0665654b613597b58d569bba5d073b1f1d4517
GABRIEL FABRICIO DA SILVA <gabriel_silva195@estudante.sesisenai.org.br>
2026-08-25 20:45:16 -0300

    feat:calc de multiplicacao

---
+++ index.js
@@ -0,0 +1,6 @@
+const numero1 = Number(prompt("Digite o primeiro número:"));
+const numero2 = Number(prompt("Digite o segundo número:"));
+
+const resultado = numero1 * numero2;
+
+alert(`O resultado da multiplicação é: ${resultado}`);

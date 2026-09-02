96a73e9fa9271215c3cb878b47965f14d1831041
MURILLO FERREIRA MARTINS <murillo_f_martins@estudante.sesisenai.org.br>
2026-09-02 20:38:42 -0300

    refatoração das operações pra usar return

--- divisao.js
+++ divisao.js
@@ -1,10 +1,8 @@
-const num1 = Number(prompt("Digite o primeiro número:"));
-const num2 = Number(prompt("Digite o segundo número:"));
-
-if (isNaN(num1) || isNaN(num2)) {
-  alert("Por favor, digite números válidos.");
-} else if (num2 === 0) {
-  alert("Erro: Não é possível dividir por zero.");
-} else {
-  alert(`O resultado da divisão é: ${num1 / num2}`);
+function dividir(num1, num2) {
+  if (isNaN(num1) || isNaN(num2) || num2 === 0) {
+    return null;
+  }
+  return num1 / num2;
 }
+
+export default dividir
--- multi.js
+++ multi.js
@@ -1,6 +1,4 @@
-const numero1 = Number(prompt("Digite o primeiro número:"));
-const numero2 = Number(prompt("Digite o segundo número:"));
-
-const resultado = numero1 * numero2;
-
-alert(`O resultado da multiplicação é: ${resultado}`);
+function multiplicar(a, b) {
+  return a * b;
+}
+export default multiplicar
